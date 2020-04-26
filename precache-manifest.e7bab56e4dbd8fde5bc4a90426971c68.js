9f9a0be129fd1cf46b9d4db0fd49d79d562a43d7
cardona <mail@joscardona.com>
2020-04-25 19:12:39 -0500

    Updates

---
+++ precache-manifest.e7bab56e4dbd8fde5bc4a90426971c68.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f34849dc7f54df2cf282ddc4f65216",
+    "url": "/link_tower_webapp/index.html"
+  },
+  {
+    "revision": "94c620c8b3aa5a73a1a2",
+    "url": "/link_tower_webapp/static/css/main.5450793a.chunk.css"
+  },
+  {
+    "revision": "5a5b1605a68d94747f37",
+    "url": "/link_tower_webapp/static/js/2.4684c82f.chunk.js"
+  },
+  {
+    "revision": "94c620c8b3aa5a73a1a2",
+    "url": "/link_tower_webapp/static/js/main.5ca64862.chunk.js"
+  },
+  {
+    "revision": "78d2dce7a278a1d7904a",
+    "url": "/link_tower_webapp/static/js/runtime~main.9573c836.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/link_tower_webapp/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/link_tower_webapp/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/link_tower_webapp/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/link_tower_webapp/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/link_tower_webapp/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/link_tower_webapp/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "0e3bcb53cbeed48bbbde73f7cafa47c1",
+    "url": "/link_tower_webapp/static/media/building.0e3bcb53.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/link_tower_webapp/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/link_tower_webapp/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/link_tower_webapp/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/link_tower_webapp/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "9e62d932da178919f1e1046d8907a90d",
+    "url": "/link_tower_webapp/static/media/homeinterior.9e62d932.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/link_tower_webapp/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/link_tower_webapp/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/link_tower_webapp/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/link_tower_webapp/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/link_tower_webapp/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/link_tower_webapp/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/link_tower_webapp/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/link_tower_webapp/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/link_tower_webapp/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/link_tower_webapp/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/link_tower_webapp/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/link_tower_webapp/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/link_tower_webapp/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
