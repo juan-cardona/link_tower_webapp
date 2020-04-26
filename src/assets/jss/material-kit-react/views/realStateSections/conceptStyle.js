c248f01f5ea921ba6b8de90c8c00387bc36a92d5
cardona <mail@joscardona.com>
2020-04-25 19:36:57 -0500

    added explore section and concept section

---
+++ src/assets/jss/material-kit-react/views/realStateSections/conceptStyle.js
@@ -0,0 +1,25 @@
+import { title } from "assets/jss/material-kit-react.js";
+
+import imagesStyle from "assets/jss/material-kit-react/imagesStyles.js";
+import { cardTitle } from "assets/jss/material-kit-react.js";
+
+const productStyle = {
+  section: {
+    padding: "70px 0",
+    textAlign: "center"
+  },
+  title: {
+    ...title,
+    marginBottom: "1rem",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none"
+  },
+  ...imagesStyle,
+  cardTitle,
+  description: {
+    color: "#999"
+  }
+};
+
+export default productStyle;
